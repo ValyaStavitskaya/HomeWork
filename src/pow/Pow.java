39701c3b4c81620f3587fba70726d04c34b90c2e
Valya-java <stav.valya@gmail.com>
2020-08-04 21:26:25 +0300

    1

---
+++ src/pow/Pow.java
@@ -0,0 +1,28 @@
+package pow;
+
+public class Pow {
+    public static void main(String[] args) {
+        System.out.println(pow(27,-3));
+
+    }
+    public static double pow(double value, double powValue) {
+        if(powValue==0) return 1;
+        if(powValue>0) {
+            if (powValue == 1) {
+                return value;
+            } else {
+                return value * pow(value, powValue - 1);
+            }
+        }
+        else {
+            if(value==0) throw new NullPointerException();
+            if (powValue == -1) {
+                return 1/value;
+            } else {
+                return 1/value * pow(value, powValue + 1);
+            }
+
+        }
+    }
+
+}
